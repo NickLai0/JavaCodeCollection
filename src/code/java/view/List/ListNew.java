8221d859c2086130ab33d6db5a60d6ee26c84e83
Nick Lai <1004289313@qq.com>
2024-11-05 20:14:16 +0800

    add ListNew

---
+++ src/code/java/view/List/ListNew.java
@@ -0,0 +1,58 @@
+package code.java.view.List;
+//: ListNew.java
+// Java 1.1 Lists are easier to use
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+//From Thinking in Java.
+public class ListNew extends Applet {
+    String flavors[] = { "Chocolate", "Strawberry",
+            "Vanilla Fudge Swirl", "Mint Chip",
+            "Mocha Almond Fudge", "Rum Raisin",
+            "Praline Cream", "Mud Pie" };
+
+    // Show 6 items, allow multiple selection:
+    List lst = new List(6, true);
+    TextArea t = new TextArea(flavors.length, 30);
+    Button b = new Button("add item");
+    int count = 0;
+    public void init() {
+        t.setEditable(false);
+        for(int i = 0; i < 4; i++)
+            lst.addItem(flavors[count++]);
+        add(t);
+        add(lst);
+        add(b);
+        lst.addItemListener(new LL());
+        b.addActionListener(new BL());
+    }
+    class LL implements ItemListener {
+        public void itemStateChanged(ItemEvent e) {
+            t.setText("");
+            String[] items = lst.getSelectedItems();
+            for(int i = 0; i < items.length; i++)
+                t.append(items[i] + "\n");
+        }
+    }
+    class BL implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if(count < flavors.length)
+                lst.addItem(flavors[count++], 0);
+        }
+    }
+    static class WL extends WindowAdapter {
+        public void windowClosing(WindowEvent e) {
+            System.exit(0);
+        }
+    }
+    public static void main(String args[]) {
+        ListNew applet = new ListNew();
+        Frame aFrame = new Frame("ListNew");
+        aFrame.addWindowListener(new WL());
+        aFrame.add(applet, BorderLayout.CENTER);
+        aFrame.setSize(300,200);
+        applet.init();
+        applet.start();
+        aFrame.setVisible(true);
+    }
+}
