23857f4c0c07c5b306f5b65d670491a0c4e4eebe
Nick Lai <1004289313@qq.com>
2024-11-15 06:57:19 +0800

    HandleClickEvent

---
+++ src/code/java/view/event/HandleClickEvent.java
@@ -0,0 +1,74 @@
+package code.java.view.event;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+/**
+ * From Core Java volume 1. Modified a little。
+ * <p>
+ * <p>
+ * Demonstrating how to handle click events
+ * by listeners.
+ * <p>
+ * A frame with a button panel.
+ */
+public class HandleClickEvent extends JFrame {
+
+    private JPanel buttonPanel;
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public HandleClickEvent() {
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+
+        // create buttons
+        var yellowButton = new JButton("Yellow");
+        var blueButton = new JButton("Blue");
+        var redButton = new JButton("Red");
+
+        buttonPanel = new JPanel();
+
+        // add buttons to panel
+        buttonPanel.add(yellowButton);
+        buttonPanel.add(blueButton);
+        buttonPanel.add(redButton);
+
+        // add panel to frame
+        add(buttonPanel);
+
+        // create button actions
+        var yellowAction = new ColorAction(Color.YELLOW);
+        var blueAction = new ColorAction(Color.BLUE);
+        var redAction = new ColorAction(Color.RED);
+
+        // associate actions with buttons
+        yellowButton.addActionListener(yellowAction);
+        blueButton.addActionListener(blueAction);
+        redButton.addActionListener(redAction);
+
+    }
+
+    /**
+     * An action listener that sets the panel's background
+     * color.
+     */
+
+    private class ColorAction implements ActionListener {
+        private Color backgroundColor;
+
+        public ColorAction(Color c) {
+            backgroundColor = c;
+        }
+
+        public void actionPerformed(ActionEvent event) {
+            buttonPanel.setBackground(backgroundColor);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        var frame = new HandleClickEvent();
+        frame.setVisible(true);
+    }
+}
