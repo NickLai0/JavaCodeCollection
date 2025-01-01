967eba04bcdddc228db25e5f6e93b616cfab6c45
Nick Lai <1004289313@qq.com>
2025-01-01 16:44:37 +0800

    add TransferHandlerTest

---
+++ src/code/java/view/transferHandler/TransferHandlerTest.java
@@ -0,0 +1,11 @@
+package code.java.view.transferHandler;
+
+import code.java.view.container.panel.color.DrawingColorToModifyText;
+
+public class TransferHandlerTest {
+
+    public static void main(String[] args) {
+        DrawingColorToModifyText.main(args);
+    }
+
+}
