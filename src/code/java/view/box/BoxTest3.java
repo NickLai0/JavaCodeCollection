e50f2327cd0e3f397ab277563110cf8355d57d77
Nick Lai <1004289313@qq.com>
2025-01-04 00:53:14 +0800

    add a spinner demo

---
+++ src/code/java/view/box/BoxTest3.java
@@ -0,0 +1,9 @@
+package code.java.view.box;
+
+import code.java.view.spinner.JSpinnerTest;
+
+public class BoxTest3 {
+    public static void main(String[] args) {
+        JSpinnerTest.main(args);
+    }
+}
