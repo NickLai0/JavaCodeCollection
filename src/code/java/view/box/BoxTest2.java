c8fe13d190eb4fdca7f8cb840dda8aa6e7a254b7
Nick Lai <1004289313@qq.com>
2025-01-03 22:12:35 +0800

    add a BoxTest2

---
+++ src/code/java/view/box/BoxTest2.java
@@ -0,0 +1,9 @@
+package code.java.view.box;
+
+import code.java.view.List.ListModelTest;
+
+public class BoxTest2 {
+    public static void main(String[] args) {
+        ListModelTest.main(args);
+    }
+}
