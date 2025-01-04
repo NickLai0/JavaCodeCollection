f22a418ae42bfe0ddaf94fbf0ce93e0a13637b73
Nick Lai <1004289313@qq.com>
2025-01-04 23:22:18 +0800

    优化SimpleJTree简单树演示案例

--- src/code/java/view/tree/SimpleJTree.java
+++ src/code/java/view/tree/SimpleJTree.java
@@ -1,54 +1,57 @@
 package code.java.view.tree;
 
+import code.java.utils.FrameUtils;
+
 import javax.swing.*;
 import javax.swing.tree.*;
+
 /**
+ * 简单树演示
+ *
  * Description:
  * 网站: <a href="http://www.crazyit.org">疯狂Java联盟</a><br>
  * Copyright (C), 2001-2018, Yeeku.H.Lee<br>
  * This program is protected by copyright laws.<br>
  * Program Name:<br>
  * Date:<br>
+ *
  * @author Yeeku.H.Lee kongyeeku@163.com
  * @version 1.0
  */
-public class SimpleJTree
-{
-	JFrame jf = new JFrame("简单树");
-	JTree tree;
-	DefaultMutableTreeNode root;
-	DefaultMutableTreeNode guangdong;
-	DefaultMutableTreeNode guangxi;
-	DefaultMutableTreeNode foshan;
-	DefaultMutableTreeNode shantou;
-	DefaultMutableTreeNode guilin;
-	DefaultMutableTreeNode nanning;
-	public void init()
-	{
-		// 依次创建树中所有节点
-		root = new DefaultMutableTreeNode("中国");
-		guangdong = new DefaultMutableTreeNode("广东");
-		guangxi = new DefaultMutableTreeNode("广西");
-		foshan = new DefaultMutableTreeNode("佛山");
-		shantou = new DefaultMutableTreeNode("汕头");
-		guilin = new DefaultMutableTreeNode("桂林");
-		nanning = new DefaultMutableTreeNode("南宁");
-		// 通过add()方法建立树节点之间的父子关系
-		guangdong.add(foshan);
-		guangdong.add(shantou);
-		guangxi.add(guilin);
-		guangxi.add(nanning);
-		root.add(guangdong);
-		root.add(guangxi);
-		// 以根节点创建树
-		tree = new JTree(root);   // ①
-		jf.add(new JScrollPane(tree));
-		jf.pack();
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
-	}
-	public static void main(String[] args)
-	{
-		new SimpleJTree().init();
-	}
+public class SimpleJTree extends JFrame {
+
+    DefaultMutableTreeNode root = new DefaultMutableTreeNode("中国");
+    DefaultMutableTreeNode guangdong = new DefaultMutableTreeNode("广东");
+    DefaultMutableTreeNode guangxi = new DefaultMutableTreeNode("广西");
+    DefaultMutableTreeNode foshan = new DefaultMutableTreeNode("佛山");
+    DefaultMutableTreeNode shantou = new DefaultMutableTreeNode("汕头");
+    DefaultMutableTreeNode guilin = new DefaultMutableTreeNode("桂林");
+    DefaultMutableTreeNode nanning = new DefaultMutableTreeNode("南宁");
+
+    public SimpleJTree() {
+        // 通过add()方法建立树节点之间的父子关系
+        setupGDProvinceNode();
+        setupGXProvinceNode();
+        root.add(guangdong);
+        root.add(guangxi);
+        // 以根节点创建树
+        add(new JScrollPane(new JTree(root)));
+        pack();
+    }
+
+    private void setupGXProvinceNode() {
+        guangxi.add(guilin);
+        guangxi.add(nanning);
+    }
+
+    private void setupGDProvinceNode() {
+        guangdong.add(foshan);
+        guangdong.add(shantou);
+    }
+
+    public static void main(String[] args) {
+        FrameUtils.visibleAndExitOnClose(SimpleJTree.class)
+                .setTitle("简单树");
+    }
+
 }
